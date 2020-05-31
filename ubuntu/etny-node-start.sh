13825d5597928baf06109bd09916a5f3236c201b
ethernity-cloud <iosif.peterfi@ethernity.cloud>
2020-05-31 07:10:15 +0000

    Updated some terms to clarify configurations settings

--- ubuntu/etny-node-start.sh
+++ ubuntu/etny-node-start.sh
@@ -5,4 +5,4 @@ cd /home/vagrant/etny/node/go-ipfs
 nohup ./ipfs daemon &
 
 cd /home/vagrant/etny/node
-nohup /home/vagrant/etny/node/etny-node.py -p $PUBLIC_KEY -k $PRIVATE_KEY -o $RESULT_PUBLIC_KEY -j $RESULT_PRIVATE_KEY &
+nohup /home/vagrant/etny/node/etny-node.py -a $ADDRESS -k $PRIVATE_KEY -r $RESULT_ADDRESS -j $RESULT_PRIVATE_KEY &
