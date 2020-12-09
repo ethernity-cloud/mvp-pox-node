c0847707d6e7cbac3045de34a2586027adf43812
ethernity-cloud <iosif.peterfi@ethernity.cloud>
2020-12-09 08:36:36 +0100

    * Updated for ipfs 0.6.0 * Added systemd service inside the qemu VM * Improved handling of IPFS downloads * Fixed to minimise gas usage * Added firewall rules to allow only blockchain   transactions, ipfs traffic and transfer from   dependencies software repositories

--- ubuntu/etny-node-provision-docker.sh
+++ ubuntu/etny-node-provision-docker.sh
@@ -9,3 +9,26 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bi
 apt-get -yq  install docker-ce
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+ufw allow in from 127.0.0.1/8
+ufw allow out to 127.0.0.1/8
+ufw allow out from any to 4.2.2.1 port 53
+ufw allow out from any to 4.2.2.2 port 53
+ufw allow out from any to 127.0.0.53 port 53
+ufw allow in 22/tcp
+
+IP=`getent hosts ipfs.ethernity.cloud | awk '{print $1}'`
+ufw allow out from any to $IP port 4001
+
+IP=`getent hosts core.bloxberg.org | awk '{print $1}'`
+ufw allow out from any to $IP port 443
+
+for IP in `dig registry-1.docker.io a | grep ^registry-1 | awk '{print $5}' | sort`; do 
+    ufw allow out from any to $IP port 443
+done
+
+for IP in `dig production.cloudflare.docker.com a | grep ^production | awk '{print $5}' | sort`; do
+    ufw allow out from any to $IP port 443
+done
+
+ufw enable
--- ubuntu/etny-node-provision-ipfs.sh
+++ ubuntu/etny-node-provision-ipfs.sh
@@ -1,7 +1,7 @@
 #1/bin/bash
 cd /home/vagrant/etny/node
-wget https://github.com/ipfs/go-ipfs/releases/download/v0.4.19/go-ipfs_v0.4.19_linux-386.tar.gz
-tar zxvf go-ipfs_v0.4.19_linux-386.tar.gz
+wget https://github.com/ipfs/go-ipfs/releases/download/v0.6.0/go-ipfs_v0.6.0_linux-386.tar.gz
+tar zxvf go-ipfs_v0.6.0_linux-386.tar.gz
 cd go-ipfs
 ./ipfs init
 ./ipfs daemon&
