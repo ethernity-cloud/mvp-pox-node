b3806e01849d6121460fdef6980c6cce96646061
ethernity-cloud <crowdsale@ethernity.cloud>
2021-02-25 07:41:25 +0000

    * Improved handling of ipfs daemon at startup

--- ubuntu/etny-node-start.sh
+++ ubuntu/etny-node-start.sh
@@ -2,14 +2,16 @@
 . /home/vagrant/etny/node/config
 
 cd /home/vagrant/etny/node/go-ipfs
-nohup ./ipfs daemon &
 
 IP=`getent hosts ipfs.ethernity.cloud | awk '{print $1}'`
 
 until ./ipfs swarm connect /ip4/$IP/tcp/4001/ipfs/QmRBc1eBt4hpJQUqHqn6eA8ixQPD3LFcUDsn6coKBQtia5
 do
-  sleep 1
+  nohup ./ipfs daemon &
+  IP=`getent hosts ipfs.ethernity.cloud | awk '{print $1}'`
+  sleep 5
 done
+
 ./ipfs bootstrap add /ip4/$IP/tcp/4001/ipfs/QmRBc1eBt4hpJQUqHqn6eA8ixQPD3LFcUDsn6coKBQtia5
 
 cd /home/vagrant/etny/node/etny-repo/node/
