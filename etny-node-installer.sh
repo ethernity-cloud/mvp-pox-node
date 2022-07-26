61cc000fd5b7324acfdabb66114915fc3945c559
Roman Kubiv <roman.k@serverbee.net>
2022-07-26 10:56:12 +0300

    fix multiple runs

--- etny-node-installer.sh
+++ etny-node-installer.sh
@@ -43,7 +43,7 @@ then
 		ubuntu_20_04_config_file_choice
 	fi
 	echo "Running ansible-playbook script..."	
-	sudo ansible-playbook -i localhost, playbook.yml -e "ansible_python_interpreter=/usr/bin/python3"	
+	sudo -E ansible-playbook -i localhost, playbook.yml -e "ansible_python_interpreter=/usr/bin/python3"	
 	if [ $? -eq 0 ]
 	then 
 		echo "Node installation completed successfully. Please allow up to 24h to see transactions on the blockchain. " && exit
@@ -140,6 +140,8 @@ cd && cd $nodefolder
 sudo -E ansible-playbook -i localhost, playbook.yml -e "ansible_python_interpreter=/usr/bin/python3"
 if [ $? -eq 0 ]
 then 
+	echo "Node installation completed successfully. Please allow up to 24h to see transactions on the blockchain. " && exit
+else 
 	echo "Restarting system. Please run the installer script afterwards to continue the setup."
 	sec=30
 	while [ $sec -ge 0 ]; do echo -n "Restarting system in [CTRL+C to cancel]: " && echo -ne "$sec\033[0K\r" && let "sec=sec-1" && sleep 1; done
