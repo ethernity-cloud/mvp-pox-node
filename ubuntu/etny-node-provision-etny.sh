009dc602617fa6491677d671bcbf5a0d4478a3cc
Mikheil <suxoza@gmail.com>
2022-08-03 23:14:35 +0400

    python libraries

--- ubuntu/etny-node-provision-etny.sh
+++ ubuntu/etny-node-provision-etny.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/vagrant/etny/node/
 git config --global http.postBuffer 524288000
-git clone https://github.com/ethernity-cloud/mvp-pox-node etny-repo
+git clone -b master-with-node-refactoring-new https://github.com/ethernity-cloud/mvp-pox-node etny-repo
 
