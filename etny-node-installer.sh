f717813dc4fc546d7f4e9c01d2772ff2c606ae66
Liviu Dănicel <ldanicel@ldcore.com>
2022-09-21 13:06:44 +0200

    Add some error checking on ansible

--- etny-node-installer.sh
+++ etny-node-installer.sh
@@ -45,7 +45,8 @@ then
 	fi
 	echo "Running ansible-playbook script..."	
 	HOME=/root
-	sudo -E ansible-playbook -i localhost, playbook.yml -e "ansible_python_interpreter=/usr/bin/python3"	
+	sudo -E ansible-playbook -i localhost, playbook.yml -e "ansible_python_interpreter=/usr/bin/python3"
+	install_result=$?
 	if [ -f $rebootfile ]
 	then 
 		echo "Restarting system. Please run the installer script afterwards to continue the setup."
@@ -53,7 +54,12 @@ then
 		while [ $sec -ge 0 ]; do echo -n "Restarting system in [CTRL+C to cancel]: " && echo -ne "$sec\033[0K\r" && let "sec=sec-1" && sleep 1; done
 		sudo reboot
 	else
-		echo "Node installation completed successfully. Please allow up to 24h to see transactions on the blockchain. " && exit
+                if [ install_result == 0 ]
+                then
+			echo "Node installation completed successfully. Please allow up to 24h to see transactions on the blockchain. " && exit
+                else
+                	echo "Node installation failed! Please check error messages above."
+                fi
 	fi
 else 
 	ubuntu_20_04_update_ansible
