931bbd029d9ae211f6b8af6bf5cfd066a98f827e
Mikheil <suxoza@gmail.com>
2022-10-07 16:57:23 +0400

    error messages again

--- node/error_messages.py
+++ node/error_messages.py
@@ -1,6 +1,6 @@
 
 
 errorMessages = {
-    'duplicated': 'Transaction with the same', #  hash has already been imported
-    'low_nonce': 'Transaction nonce is too low' # Transaction nonce is too low. Try incrementing the nonce.
+    'duplicated': 'hash has already been imported', 
+    'low_nonce': 'Transaction nonce is too low. Try incrementing the nonce'
 }
