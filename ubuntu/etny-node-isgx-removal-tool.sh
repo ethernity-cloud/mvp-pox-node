3a6a607032a34fbd5396d3c92294a5b39b3dcdf6
apoeana <103731835+apoeana@users.noreply.github.com>
2023-07-11 19:11:08 +0300

    isgx fix (#70)
    * isgx fix
    
    * isgx fix
    
    * Delete etny-node-provision-sgx.sh
    
    * isgx fix
    
    * Delete ubuntu/ubuntu directory
    
    * isgx fix
    
    * isgx fix
    
    * isgx fix
    
    * isgx fix

--- ubuntu/etny-node-isgx-removal-tool.sh
+++ ubuntu/etny-node-isgx-removal-tool.sh
@@ -5,19 +5,20 @@ if test $(id -u) -ne 0; then
     exit 1
 fi
 
-echo "Find the directory where Vagrant is located...."
+echo "Checking Vagrant VM for sgx_enclave and isgx..."
 vagrant_dir=$(sudo vagrant global-status | grep running | awk '{print $5}')
 if [ -z "$vagrant_dir" ]; then
     echo "No running Vagrant environment found."
 else
-    echo "Checking Vagrant VM for sgx_enclave and isgx..."
-
+    echo "Vagrant VM:"
+    # Commands executed on the Vagrant VM
     cd "$vagrant_dir"
     sudo vagrant ssh -c "
-        # Start of uninstall script on the Vagrant VM
-        if [ -c /dev/sgx_enclave ] && [ -c /dev/isgx ]; then
-            echo 'Both sgx_enclave and isgx drivers are present on the Vagrant VM.'
-            echo 'Running uninstall script on the Vagrant VM...'
+        echo 'Checking for sgx_enclave and isgx drivers...'
+        # Check for sgx_enclave driver
+        if ls /dev/ | grep -q 'sgx_enclave'; then
+            echo '/dev/sgx_enclave driver is present on the Vagrant VM.'
+            echo 'Uninstalling isgx driver...'
 
             # Check if the AESM service is running
             if sudo service aesmd status 2>/dev/null | grep 'Active: active (running)'; then
@@ -53,41 +54,54 @@ else
 
             echo 'Uninstall script executed successfully on the Vagrant VM.'
         else
-            echo 'Checking for SGX drivers on the Vagrant VM...'
-            if ls /dev/ | grep -q 'sgx_enclave'; then
-                echo '/dev/sgx_enclave driver is present on the Vagrant VM.'
-            fi
+            echo 'sgx_enclave driver is not present on the Vagrant VM.'
+
+            # Check for isgx driver
             if ls /dev/ | grep -q 'isgx'; then
                 echo '/dev/isgx driver is present on the Vagrant VM.'
+            else
+                echo 'isgx driver is not present on the Vagrant VM.'
+                echo 'Running install script on the Vagrant VM...'
+                sudo apt-get -yq update
+                sudo apt-get -yq install make gcc
+                cd /tmp
+                sudo wget https://github.com/ethernity-cloud/sgx-driver/raw/main/sgx_linux_x64_driver_2.11.054c9c4c.bin
+                sudo chmod +x sgx_linux_x64_driver_2.11.054c9c4c.bin
+                sudo ./sgx_linux_x64_driver_2.11.054c9c4c.bin
+                echo 'Install script executed successfully on the Vagrant VM.'
             fi
         fi
     "
+fi
 
 echo "Checking host for sgx_enclave and isgx..."
-if [ -c /dev/sgx_enclave ] && [ -c /dev/isgx ]; then
-    echo "Both sgx_enclave and isgx drivers are present on the host."
-    echo "Running uninstall script on the host..."
+echo "Host:"
+# Commands executed on the host
+# Check for sgx_enclave driver
+if ls /dev/ | grep -q 'sgx_enclave'; then
+    echo '/dev/sgx_enclave driver is present on the host.'
+    echo 'Uninstalling isgx driver...'
 
-   # Check if the AESM service is running
-   if sudo service aesmd status 2>/dev/null | grep 'Active: active (running)'; then
-           echo -e 'Uninstall failed on the host!'
-           echo -e '\nPlease stop the AESM service and uninstall the PSW package first on the host'
-           exit 1
-   fi
+    # Check if the AESM service is running
+    if sudo service aesmd status 2>/dev/null | grep 'Active: active (running)'; then
+        echo -e 'Uninstall failed on the host!'
+        echo -e '\nPlease stop the AESM service and uninstall the PSW package first on the host'
+        exit 1
+    fi
 
     # Removing the kernel module if it is inserted
     sudo modinfo isgx &> /dev/null
-    if [[ $? == "0" ]]; then
+    if [[ $? == '0' ]]; then
         sudo modprobe -r isgx
-        if [[ $? != "0" ]]; then
-            echo -e "\nUninstall failed on the host because the kernel module is in use"
+        if [[ $? != '0' ]]; then
+            echo -e '\nUninstall failed on the host because the kernel module is in use'
             exit 1
         fi
     fi
 
     # Removing the .ko file
-    sudo find /usr/lib/modules/ -name "isgx.ko" -exec rm -f {} +
-    
+    sudo find /usr/lib/modules/ -name 'isgx.ko' -exec rm -f {} +
+
     # Removing from depmod
     sudo depmod
 
@@ -100,16 +114,23 @@ if [ -c /dev/sgx_enclave ] && [ -c /dev/isgx ]; then
     # Removing the current folder
     sudo rm -fr /opt/intel/sgxdriver
 
-    echo "Uninstall script executed successfully on the host."
+    echo 'Uninstall script executed successfully on the host.'
 else
-    echo "Checking for SGX drivers on the host..."
-    if ls /dev/ | grep -q "sgx_enclave"; then
-        echo "/dev/sgx_enclave driver is present on the host."
+    echo 'sgx_enclave driver is not present on the host.'
+
+    # Check for isgx driver
+    if ls /dev/ | grep -q 'isgx'; then
+        echo '/dev/isgx driver is present on the host.'
+    else
+        echo 'isgx driver is not present on the host.'
+        echo 'Running install script on the host...'
+        sudo apt-get -yq update
+        sudo apt-get -yq install make gcc
+        cd /tmp
+        sudo wget https://github.com/ethernity-cloud/sgx-driver/raw/main/sgx_linux_x64_driver_2.11.054c9c4c.bin
+        sudo chmod +x sgx_linux_x64_driver_2.11.054c9c4c.bin
+        sudo ./sgx_linux_x64_driver_2.11.054c9c4c.bin
     fi
-    if ls /dev/ | grep -q "isgx"; then
-        echo "/dev/isgx driver is present on the host."
-    fi
-fi
 fi
 
 echo "Operation completed successfully!"
