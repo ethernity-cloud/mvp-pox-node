c1dff3e0d5e31e534e7bba9e86eeb5bb515f15d8
apoeana <103731835+apoeana@users.noreply.github.com>
2023-07-27 11:45:38 +0300

    Update etny-node-installer.sh

--- etny-node-installer.sh
+++ etny-node-installer.sh
@@ -60,7 +60,7 @@ task_price_check() {
         echo "Task execution price already exists in the config file and is currently set to $current_price ETNY/hour."
         echo "Would you like to modify it? (y/N)"
         read modify
-	if [[ -z "$modify" ]] || [[ "$modify" =~ ^[Yy]$ ]]; then
+	if [[ -z "$modify" ]] || [[ "$modify" =~ ^[Nn]$ ]]; then
             set_task_price
         fi
     else
