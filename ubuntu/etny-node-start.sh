e5891157c172f6c22271b78f851193cfa0d36044
apoeana <103731835+apoeana@users.noreply.github.com>
2024-05-27 09:38:58 +0300

    Custom_RPC
    Custom_RPC

--- ubuntu/etny-node-start.sh
+++ ubuntu/etny-node-start.sh
@@ -57,10 +57,9 @@ then
 fi
 
 cd /home/vagrant/etny/node/etny-repo/node/
-#git fetch origin
-#git reset --hard origin/master
-#git pull
-git checkout Custom_RPC
+git fetch origin
+git reset --hard origin/master
+git pull
 
 COMMAND_LINE="/home/vagrant/etny/node/etny-repo/node/etny-node.py -a ${ADDRESS} -k ${PRIVATE_KEY} -r ${RESULT_ADDRESS} -j ${RESULT_PRIVATE_KEY} -v ${TASK_EXECUTION_PRICE} -n ${NETWORK} -i ${IPFS_HOST} -l ${IPFS_LOCAL}"
 
