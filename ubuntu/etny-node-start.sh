e700a3385105cef63c10981404963f97ccce24cc
apoeana <103731835+apoeana@users.noreply.github.com>
2024-05-30 09:08:45 +0300

    git checkout local-ipfs

--- ubuntu/etny-node-start.sh
+++ ubuntu/etny-node-start.sh
@@ -58,9 +58,10 @@ then
 fi
 
 cd /home/vagrant/etny/node/etny-repo/node/
-git fetch origin
-git reset --hard origin/master
-git pull
+#git fetch origin
+#git reset --hard origin/master
+#git pull
+git checkout local-ipfs
 
 COMMAND_LINE="/home/vagrant/etny/node/etny-repo/node/etny-node.py -a ${ADDRESS} -k ${PRIVATE_KEY} -r ${RESULT_ADDRESS} -j ${RESULT_PRIVATE_KEY} -v ${TASK_EXECUTION_PRICE} -n ${NETWORK} -i ${IPFS_HOST} -l ${IPFS_LOCAL}"
 
